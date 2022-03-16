4d2e8608d6aa948f844daaf96083e4b98bcdd4f7
Mira Horejsi <mira.horejsi@gmail.com>
2022-03-16 20:47:15 +0100

    created method "sum"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Test");
+        System.out.println("Součet: " + sum(1.3, 2.2, -4));
 
+    }
+
+    public static double sum(double ... doubles){
+        double sum = 0;
+
+        for(double a : doubles) sum += a;
+
+            return sum;
     }
 
 }
