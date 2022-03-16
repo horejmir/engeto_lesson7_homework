3da996efd055119dc47fefb169b6c50fc3cd21c7
Mira Horejsi <mira.horejsi@gmail.com>
2022-03-16 21:19:34 +0100

    added tests for method sum

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testSumOk1(){
+
+        assertEquals(2.1, Main.sum(2.1));
+    }
+
+    @Test
+    public void testSumOk2(){
+        assertEquals(2.6, Main.sum(2.1, 0.5));
+    }
+
+    @Test
+    public void testSumOk3(){
+        assertEquals(10.0, Main.sum(1,2,3,4));
+    }
+
+}
