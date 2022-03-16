fda33054c51c58f923327707c176cfa57b303d4e
Mira Horejsi <mira.horejsi@gmail.com>
2022-03-16 21:25:58 +0100

    code formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Součet: " + sum(1.3, 2.2, -4));
-
     }
 
-    public static double sum(double ... doubles){
+    public static double sum(double... doubles){
         double sum = 0;
 
         for(double a : doubles) sum += a;
 
-            return sum;
+        return sum;
     }
 
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,13 +6,12 @@ public class MainTest {
 
     @Test
     public void testSumOk1(){
-
         assertEquals(2.1, Main.sum(2.1));
     }
 
     @Test
     public void testSumOk2(){
-        assertEquals(2.6, Main.sum(2.1, 0.5));
+        assertEquals(1.6, Main.sum(2.1, -0.5));
     }
 
     @Test
